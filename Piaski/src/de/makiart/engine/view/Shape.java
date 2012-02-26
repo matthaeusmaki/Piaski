c3124985397fe7b2fdf8e8f062674a3822f6d109
Thomas Echerer <Thomas.echerer@gmail.com>
2012-02-26 22:00:50 +0100

    implements Shapes and specialy sqaure

---
+++ Piaski/src/de/makiart/engine/view/Shape.java
@@ -0,0 +1,100 @@
+package de.makiart.engine.view;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public abstract class Shape {
+	
+	//	Buffers
+	public 		FloatBuffer 	verticesBuffer;
+	public 		ShortBuffer 	indicesBuffer;
+	public	 	FloatBuffer 	colorBuffer;
+	public	 	float[]			rgba;	
+	private 	int 			numOfIndices;
+	
+	//	Translate params
+	private		float			x 	= 	0;
+	private 	float			y	=	0;
+	private		float			z	=	0;
+	
+	//	Rotate Perams
+	private 	float			rx	=	0;
+	private		float			ry	=	0;
+	private		float			rz	=	0;
+	
+	
+
+	protected void setVertices(float[] vertices) {
+		// a float is 4 bytes, therefore we multiply the number if
+		// vertices with 4.
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+		vbb.order(ByteOrder.nativeOrder());
+		verticesBuffer = vbb.asFloatBuffer();
+		verticesBuffer.put(vertices);
+		verticesBuffer.position(0);
+	}
+	 
+	protected void setIndices(short[] indices) {
+		// short is 2 bytes, therefore we multiply the number if
+		// vertices with 2.
+		ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * 2);
+		ibb.order(ByteOrder.nativeOrder());
+		indicesBuffer = ibb.asShortBuffer();
+		indicesBuffer.put(indices);
+		indicesBuffer.position(0);
+		numOfIndices = indices.length;
+	}
+	 
+    protected void setColor(float red, float green, float blue, float alpha) {
+        // Setting the flat color.
+        rgba[0] = red;
+        rgba[1] = green;
+        rgba[2] = blue;
+        rgba[3] = alpha;
+    }	 
+    
+	/*
+    protected void setColors(float[] colors) {
+		// float has 4 bytes.
+		ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+		cbb.order(ByteOrder.nativeOrder());
+		colorBuffer = cbb.asFloatBuffer();
+		colorBuffer.put(colors);
+		colorBuffer.position(0);
+    }
+    */
+    
+    
+    public void draw(GL10 gl) {
+    	gl.glVertexPointer(3, GL10.GL_FLOAT, 0, verticesBuffer);
+        //gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
+    	gl.glColor4f(rgba[0], rgba[1], rgba[2], rgba[3]);
+    	
+    
+
+        gl.glDrawElements(GL10.GL_TRIANGLES, numOfIndices, GL10.GL_UNSIGNED_SHORT, indicesBuffer);
+    	
+    	//Disable the client state before leaving
+    	gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+    }
+
+    public void setPosition( int X, int Y) {
+    	x = X;
+    	y = Y;    	
+    }
+    
+    public float getX() {
+    	return x;
+    }
+    
+    public float getY() {
+    	return y;
+    }
+    
+
+}
---
+++ Piaski/src/de/makiart/engine/view/Square.java
@@ -0,0 +1,64 @@
+package de.makiart.engine.view;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Square extends Shape {
+	
+	public Square() {
+		float vertices[] = {
+				-1.0f, -1.0f,  0.0f,       // V1 - bottom left				
+				-1.0f,  1.0f,  0.0f,       // V2 - top left
+				1.0f, -1.0f,  0.0f,        // V3 - bottom right
+				1.0f,  1.0f,  0.0f         // V4 - top right
+		};
+		super.setVertices(vertices);
+		
+		super.setColor(0.0f, 1.0f, 0.0f, 0.5f);
+		
+		short[]	indices	=	new short[] {
+				3, 1, 0,
+				0, 2, 3
+		};
+		super.setIndices(indices);
+		
+		
+	}
+	
+	public Square(float[] vertices ) {
+		super.setVertices(vertices);
+		
+		short[]	indices	=	new short[] {
+				3, 1, 0,
+				0, 2, 3
+		};		
+		super.setIndices(indices);
+		
+		
+		
+	}
+	/*
+	public Square(float[] vertices, short[] indices, float[] colors ) {
+		super.setVertices(vertices);
+		super.setIndices(indices);
+		super.setColors(colors);
+	}
+	*/
+	/*
+	public void draw(GL10 gl) {
+    	gl.glVertexPointer(3, GL10.GL_FLOAT, 0, super.verticesBuffer);
+        //gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
+    	gl.glColor4f(super.rgba[0], super.rgba[1], super.rgba[2], super.rgba[3]);
+    	
+    	// Draw the vertices as triangle strip
+    	gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+
+        //gl.glDrawElements(GL10.GL_TRIANGLES, numOfIndices, GL10.GL_UNSIGNED_SHORT, indicesBuffer);
+    	
+    	//Disable the client state before leaving
+    	gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+    }
+    */
+	
+	
+}
