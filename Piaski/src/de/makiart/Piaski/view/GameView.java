ae8b873f3e0fba33f44ece4611f7a49be942c8ca
Thomas Echerer <Thomas.Echerer@gmail.com>
2012-03-24 16:24:26 +0100

    Cleane version: Entferenen von Button-Tests und korrektur der MenuView in GameView

---
+++ Piaski/src/de/makiart/Piaski/view/GameView.java
@@ -0,0 +1,23 @@
+package de.makiart.Piaski.view;
+
+import de.makiart.engine.view.AbstractView;
+import de.makiart.engine.view.Square;
+
+public class GameView extends AbstractView
+ {
+
+	//private ArrayList<Button> Objekte 	=	new ArrayList<Button>();
+	
+	
+	public GameView() {
+		addComponent(new Square());
+	
+	}
+	
+
+
+	
+
+
+	
+}
